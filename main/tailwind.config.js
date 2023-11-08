4231a170e349c4367d2402a0537f24e5a1186cee
Web File <126077909+WebKelas8A@users.noreply.github.com>
2023-11-08 19:28:27 +0700

    Add files via upload

---
+++ main/tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    content: ['index.html'],
+    darkMode: 'class',
+    theme: {
+      container: {
+        center: true,
+        padding: '16px',
+      },
+      extend: {
+        colors: {
+          primary: '#2e2e2e',
+          secondary: '#64748b',
+          dark: '#2e2e2e',
+        },
+        screens: {
+          '2xl': '1320px',
+        },
+      },
+    },
+    plugins: [],
+  };
